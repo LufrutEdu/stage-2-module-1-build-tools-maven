7e0ee3967283d87a36ebaa8c1d396a95ba043511
Lufrut <superkolasar5@gmail.com>
2023-08-21 19:46:08 +0200

    finish task

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,30 @@
 package com.epam.demo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-       //magic happens here
+        Predicate<String> check = str -> {
+            if(str.contains("-")) return false;
+            else if(str.contains("0")) return false;
+            else if(str.isEmpty()) return false;
+            else return true;
+        };
+        for (String item:
+                args) {
+            if(item == null) return false;
+            if(!check.test(item)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<String> str = new ArrayList<>();
+        str.add("2");
+        str.add(null);
+        System.out.println(isAllPositiveNumbers(str));
     }
 }
+
