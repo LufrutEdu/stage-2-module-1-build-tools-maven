287a34dacead0897b47f20ea543ebbd68214bae3
Lufrut <superkolasar5@gmail.com>
2023-09-11 18:51:43 +0200

    fixed all errors

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,23 +1,15 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        Predicate<String> check = str -> {
-            if(str.contains("-")) return false;
-            else if(str.contains("0")) return false;
-            else if(str.isEmpty()) return false;
-            else return true;
-        };
-        for (String item:
-                args) {
-            if(item == null) return false;
-            if(!check.test(item)) return false;
-        }
-        return true;
+
+        return args.stream().allMatch(StringUtils::isPositiveNumber);
     }
 
 }
